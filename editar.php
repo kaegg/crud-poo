f6e74f58ce87f1899a5beead4cae7298daafd52d
Kauan <kauan.eguchi@hotmail.com>
2023-05-26 14:32:42 -0300

    estilização index e editar

--- editar.php
+++ editar.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'vendor/autoload.php';
+require_once 'App/view/header.php';
 
 $pegar_id = $_GET['id'];
 $produto = new \App\Model\Produto();
@@ -10,22 +11,41 @@ $desc_prod = new \App\Model\ProdutoDao();
 
 $produto = $desc_prod->read();
 
-echo "<pre>";
-print_r($produto);
+foreach($produto as $produtos){
+
+  if($produtos['id'] == $pegar_id){
+    $prod = $produtos;
+  }
+
+}
 
 ?>
 
-<form action="" method="post">
-  Nome:<input type="text" name="nome" id="">
-  Descrição:<input type="text" name="descricao" id="">
-  <button type="submit" value="atualizar">Atualizar</button>
-</form>
+<h1 class="text-center mb-3 mt-5">Editar <?php echo $prod['nome']; ?></h1>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4 offset-sm-4">
+      <form action="" method="post" class="text-center">
+        <div class="input-group flex-nowrap mb-4">
+          <span class="input-group-text" id="addon-wrapping">Produto</span>
+          <input class="form-control form-control-sm" type="text" name="nome"><br>
+        </div>
+        <div class="input-group flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping">Descrição</span>
+          <input class="form-control form-control-sm" type="text" name="descricao"><br>
+        </div>
+        <button type="submit" value="atualizar" class="btn btn-success mt-4 mb-5">Editar</button>
+      </form>
+    </div>
+  </div>
+</div>
 
 <?php
 
 $prod_atualizado = new \App\Model\Produto();
-$nome = $_POST['nome'];
-$desc = $_POST['descricao'];
+$nome = $_POST['nome'] ?? '';
+$desc = $_POST['descricao'] ?? '';
 $prod_atualizado->setNome($nome);
 $prod_atualizado->setDesc($desc);
 $prod_atualizado->setId($pegar_id);
@@ -36,5 +56,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   }
 }
 
-echo "<pre>";
-print_r($prod_atualizado);
+
+// echo "<pre>";
+// print_r($prod_atualizado);
--- index.php
+++ index.php
@@ -1,39 +1,83 @@
 <?php
 
 require_once 'vendor/autoload.php';
+require_once 'App/view/header.php';
+
+$nome = $_POST['nome'] ?? '';
+$desc = $_POST['descricao'] ?? '';
 
 $produto = new \App\Model\Produto();
-$produto->setNome('Notebook');
-$produto->setDesc('i5, 8gb, 256gb SSD');
+$produto->setNome($nome);
+$produto->setDesc($desc);
 
-$produto2 = new \App\Model\Produto();
-$produto2->setNome('Mouse');
-$produto2->setDesc('Razer');
-
-$produto3 = new \App\Model\Produto();
-$produto3->setNome('Escrivaninha');
-$produto3->setDesc('Madeira, largra: 1,50');
-
-$produto4 = new \App\Model\Produto();
-$produto4->setId(9);
-$produto4->setNome('Microfone AT2020');
-$produto4->setDesc('Microfone Condensador');
-
-$produtoDao = new \App\Model\ProdutoDao();
-// $produtoDao->create($produto);
-// $produtoDao->create($produto2);
-// $produtoDao->create($produto3);
-$produtoDao->delete($produto4);
-$produtoDao->read();
-
-foreach($produtoDao->read() as $produto){
-  echo $produto['nome'] . "<br>" . $produto['descrição'] . "<br>" . "<a href='editar.php?id=" . $produto['id'] . "'>Editar</a>" . " " . "<a href='deletar.php?id=" . $produto['id'] . "'>Deletar</a>" . "<hr>";
-}
+$criar = new \App\Model\ProdutoDao();
 
 ?>
 
-<form action="" method="post">
-  Nome:<input type="text" name="nome" id=""><br>
-  Descrição:<input type="text" name="descricao" id=""><br>
-  <button type="submit" value="atualizar">Adicionar</button>
-</form>
+<h1 class="text-center mb-3 mt-5">Controle de produtos</h1>
+
+<?php
+  if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    if($_POST['nome'] != "" && $_POST['descricao'] != ""){
+    
+      $criar->create($produto);
+      header("Location: index.php");
+      exit;
+    
+    }else{
+    
+      echo "
+      <div class='col-sm-4 offset-sm-4 text-center'>
+        <div class='alert alert-danger' role='alert'>
+          <h5 class='text-center mb-0'>O campo nome e descrição devem estar preenchidos</h5>
+        </div>
+      </div>";
+    
+    }
+  
+  }
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4 offset-sm-4">
+      <form action="" method="post" class="text-center">
+        <div class="input-group flex-nowrap mb-4">
+          <span class="input-group-text" id="addon-wrapping">Produto</span>
+          <input class="form-control form-control-sm" type="text" name="nome"><br>
+        </div>
+        <div class="input-group flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping">Descrição</span>
+          <input class="form-control form-control-sm" type="text" name="descricao"><br>
+        </div>
+        <button type="submit" value="adicionar" class="btn btn-success mt-4 mb-5">Adicionar</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+<?php
+
+echo "
+  <div class='container text-center'>
+    <div class='row'>";
+      
+foreach ($criar->read() as $produto) {
+  echo "
+    <div class='col-md-4 mb-4'>
+      <div class='card'>
+        <h4 class='text-center mt-4'>{$produto['nome']}</h4>
+        <h6 class='text-center'>id={$produto['id']}</h6>
+        <p class='text-center'>{$produto['descrição']}</p>
+        <div class='d-flex flex-row justify-content-center mb-4'>
+          <a href='editar.php?id={$produto['id']}' class='text-light text-decoration-none me-3'><button class='btn btn-primary'>Editar</button></a>
+          <a href='deletar.php?id={$produto['id']}' class='text-light text-decoration-none ms-3'><button class='btn btn-danger'>Deletar</button></a>
+        </div>
+      </div>
+    </div>";
+}
+
+echo "
+    </div>   
+  </div>";
